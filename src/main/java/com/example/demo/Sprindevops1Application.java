d75ee49dd785e4deaae4a2bed22f26c80eccf8ab
YDPRJY <ydprjy@gmail.com>
2026-02-17 10:48:41 +0530

    first commit

---
+++ src/main/java/com/example/demo/Sprindevops1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprindevops1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprindevops1Application.class, args);
+	}
+
+}
